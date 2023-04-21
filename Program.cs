a8952448571be01522f728bd4a439e81134a3964
namduong2006 <129411334+namduong2006@users.noreply.github.com>
2023-04-21 20:12:59 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LopHCN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            double width, height;
+            Console.WriteLine("Tính chu vi, diện tích HCN.");
+            Console.WriteLine("Nhập chiều rộng < chiều dài.");
+            do
+            {
+                Console.Write("Nhập chiều rộng HCN :");
+                width = double.Parse(Console.ReadLine());
+                Console.Write("Nhập chiều dài HCN :");
+                height = double.Parse(Console.ReadLine());
+            }
+            while (width > height);
+            Rectangle rectangle = new Rectangle(width,height);
+            Console.WriteLine(rectangle.Display());
+            Console.WriteLine($"Diện tích HCN :{rectangle.GetArena()} ");
+            Console.WriteLine($"Chu vi HCN : {rectangle.GetPerimeter()}");
+            Console.ReadLine();            
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LopHCN
+{
+    internal class Rectangle
+    {
+        double width, height;
+        public Rectangle()
+        {
+        }
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public double GetArena()
+        {
+           return width * height;
+        }
+        public double GetPerimeter()
+        {
+            return (width + height) * 2;
+        }
+        public string Display()
+        {
+            return "HCN{" + "chiều rộng =" + width + ", chiều dài =" + height + "}";
+        }
+    }
+}
